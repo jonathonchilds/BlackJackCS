0d815514fea869af13cd04ea0fa3cda451f7db79
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2021-12-05 19:15:24 -0500

    working code for creating a deck of 52 cards

--- Program.cs
+++ Program.cs
@@ -3,78 +3,79 @@ using System.Collections.Generic;
 
 namespace BlackJackCS
 {
-
-    // create deck of 52 cards & assign to a list
-    // cards have suit and rank
-    class CreateDeck
-    {
-        var cardSuits = new List<string>() { "Spades", "Hearts", "Clubs", "Diamonds" };
-        var cardRanks = new List<string>() { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace");
-
-
-    }
-
-
-    // shuffle deck of cards & assign to a list
-    // assign top two items to playersHand
-    // assign next two items to dealersHand
-
-    // display playersHand to player
-
-    // give player option to "hit"
-    //if player hits, extract next card from shuffled list
-    // add that card to players hand
-    // if total of players hand remains <21, player may hit again
-    // if total of players hand >21, automatic loss for player
-
-    // if player "stands" 
-    // dealer hits until dealers hand >= 17
-    // if dealers hand >21, automatic loss for dealer
-
-    // if playersHand > dealersHand 
-    // then user wins
-
-    // if dealersHand >= playersHand 
-    // then dealer wins
-
-    // prompt option to play again
-
-    // if playing again 
-    // clear both hands
-    // reshuffle deck
-
     class Program
     {
         static void Main(string[] args)
         {
+            var cardSuits = new List<string>() { "Spades", "Hearts", "Clubs", "Diamonds" };
+            var cardRanks = new List<string>() { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+            var deck = new List<string>();
 
-            var deck = new List<string>() {
-              "Ace of hearts", "2 of hearts", "3 of hearts", "4 of hearts", "5 of hearts", "6 of hearts", "7 of hearts", "8 of hearts", "9 of hearts", "10 of hearts",
-            "Jack of hearts", "Queen of hearts", "King of hearts", "Ace of diamonds", "2 of diamonds", "3 of diamonds", "4 of diamonds", "5 of diamonds", "6 of diamonds", "7 of diamonds", "8 of diamonds", "9 of diamonds",
-             "10 of diamonds", "Jack of diamonds", "Queen of diamonds", "King of diamonds", "Ace of spades", "2 of spades", "3 of spades", "4 of spades", "5 of spades", "6 of spades", "7 of spades", "8 of spades", "9 of spades", "10 of spades",
-             "Jack of spades", "Queen of spades", "King of spades", "Ace of clubs", "2 of clubs", "3 of clubs", "4 of clubs", "5 of clubs", "6 of clubs", "7 of clubs", "8 of clubs", "9 of clubs", "10 of clubs",
-            "Jack of clubs", "Queen of clubs", "King of clubs"
-            };
-
-            var deckLength = deck.Count;
-
-            for (var rightIndex = deckLength - 1; rightIndex >= 1; rightIndex--)
+            foreach (var suit in cardSuits)
             {
-                var randomNumberGenerator = new Random();
-                var leftIndex = randomNumberGenerator.Next(rightIndex);
-
-                var leftCard = deck[leftIndex];
-                var rightCard = deck[rightIndex];
-                deck[rightIndex] = leftCard;
-                deck[leftIndex] = rightCard;
+                foreach (var rank in cardRanks)
+                {
+                    var card = ($"{rank} of {suit}");
+                    deck.Add(card);
+                }
             }
-            var firstCard = deck[0];
-            Console.WriteLine(firstCard);
-            var secondCard = deck[1];
-            Console.WriteLine(secondCard);
+            foreach (var card in deck)
+            {
+                Console.WriteLine(card);
+            }
+
+
+            // for (var rightIndex = deckLength - 1; rightIndex >= 1; rightIndex--)
+            // {
+            //     var randomNumberGenerator = new Random();
+            //     var leftIndex = randomNumberGenerator.Next(rightIndex);
+
+            //     var leftCard = deck[leftIndex];
+            //     var rightCard = deck[rightIndex];
+            //     deck[rightIndex] = leftCard;
+            //     deck[leftIndex] = rightCard;
+            // }
+            // var firstCard = deck[0];
+            // Console.WriteLine(firstCard);
+            // var secondCard = deck[1];
+            // Console.WriteLine(secondCard);
         }
     }
 }
+
+
+
+// shuffle deck of cards & assign to a list
+// assign top two items to playersHand
+// assign next two items to dealersHand
+
+// display playersHand to player
+
+// give player option to "hit"
+//if player hits, extract next card from shuffled list
+// add that card to players hand
+// if total of players hand remains <21, player may hit again
+// if total of players hand >21, automatic loss for player
+
+// if player "stands" 
+// dealer hits until dealers hand >= 17
+// if dealers hand >21, automatic loss for dealer
+
+// if playersHand > dealersHand 
+// then user wins
+
+// if dealersHand >= playersHand 
+// then dealer wins
+
+// prompt option to play again
+
+// if playing again 
+// clear both hands
+// reshuffle deck
+
+
+
+
 
 
 // using System;
